fbf62ece8d80cef3a4edc4a4724f846fbee787c9
Bassel Madarati <basselmadarati@gmail.com>
2023-09-10 15:03:37 +0300

    ...

--- exercise8-react-higher-order-component/src/App.js
+++ exercise8-react-higher-order-component/src/App.js
@@ -1,22 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+const withMousePosition = (OrginalComponent) => {
+  const NewComponent = (props) => {
+    const [mousePosition, setMousePosition] = useState({
+      x: 0, y: 0
+    })
+    useEffect(() => {
+      const handleMousePositionChange = (e) => {
+        setMousePosition({
+          x: e.clientX, y: e.clientY,
+        })
+      }
+      window.addEventListener("mousemove", handleMousePositionChange);
+      return () => {
+        window.removeEventListener("mousemove", handleMousePositionChange)
+      }
+    }, [])
+    console.log({ ...props })
+    return (
+      <OrginalComponent {...props} mousePosition={mousePosition} />
+    )
+  }
+  return NewComponent
+}
+
+const PanelMouseLogger = ({ children, mousePosition }) => {
+  if (!mousePosition) {
+    return null;
+  }
+  return (
+    <div>
+      {children}
+      <div>
+        <span>X:{mousePosition.x} </span>
+        <span>y:{mousePosition.y}</span>
+      </div>
+    </div>
+  )
+}
+const PointMouseLogger = ({ mousePosition }) => {
+  if (!mousePosition) {
+    return null;
+  }
+  return (
+    <p>({mousePosition.x},{mousePosition.y})</p>
+  )
+}
+const PanelMouseTracker = withMousePosition(PanelMouseLogger)
+const PointMouseTracker = withMousePosition(PointMouseLogger)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <PanelMouseTracker>
+          <p>Mouse Position:</p>
+        </PanelMouseTracker>
+        <PointMouseTracker />
       </header>
     </div>
   );
